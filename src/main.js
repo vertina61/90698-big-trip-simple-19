7f2dd8533a017bcc711df83bb01381c2e316778e
vertina61 <natalya.vertieva@mail.ru>
2023-01-09 23:38:34 +0300

    2.15. Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import FilterView from './view/filter-view.js';
 import ListPresenter from './presenter/list-presenter.js';
 import {render} from './render.js';
+import PointsModel from './model/points-model.js';
 
 const sitePageElement = document.querySelector('.page-body');
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteBoardContainerElement = sitePageElement.querySelector('.trip-events');
-const listPresenter = new ListPresenter({boardContainer: siteBoardContainerElement});
+const pointsModel = new PointsModel();
+const listPresenter = new ListPresenter({boardContainer: siteBoardContainerElement, pointsModel});
 
 render(new FilterView(), siteHeaderElement);
 listPresenter.init();
---
+++ src/mock/destination.js
@@ -0,0 +1,22 @@
+import { getRandomArrayElement, getRandomInteger } from '../utils.js';
+import { DESCRIPTION, CITY_NAMES } from '../const.js';
+
+const createDestinations = (count) => {
+  const array = [];
+  for (let index = 0; index < count; index++) {
+    array.push({
+      id: index + 1,
+      description: getRandomArrayElement(DESCRIPTION),
+      name: getRandomArrayElement(CITY_NAMES),
+      pictures: [
+        {
+          src: `https://loremflickr.com/248/152?random=${getRandomInteger(1, 10)}`,
+          description: getRandomArrayElement(DESCRIPTION),
+        }
+      ]
+    });
+  }
+  return array;
+};
+
+export const destinations = createDestinations(10);
