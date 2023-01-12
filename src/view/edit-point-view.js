bb360385b91910362d78219df97c705ecfbbb506
vertina61 <natalya.vertieva@mail.ru>
2023-01-13 00:00:28 +0300

    3.10. Революция или эволюция? (часть 1)

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -98,23 +98,26 @@ function createEditPointTemplate(point) {
 }
 
 export default class EditPointView {
+  #element = null;
+  #point = null;
+
   constructor({point}) {
-    this.point = point;
+    this.#point = point;
   }
 
-  getTemplate() {
-    return createEditPointTemplate(this.point);
+  get template() {
+    return createEditPointTemplate(this.#point);
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
