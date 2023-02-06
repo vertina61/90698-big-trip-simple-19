eb577bfbff31e1217086ad3141a47ce71838821f
vertina61 <natalya.vertieva@mail.ru>
2023-02-06 22:56:54 +0300

    8.1.2 Добавит модуль TasksApiService в точке входа

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import { render } from './framework/render.js';
+import PointsApiService from './point-api-service.js';
 
-const pointsModel = new PointsModel();
+const AUTHORIZATION = 'Basic gB5haM57wcl56za2j';
+const END_POINT = 'https://19.ecmascript.pages.academy/big-trip-simple';
+
+const pointsModel = new PointsModel({
+  pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)
+});
 const filterModel = new FilterModel();
 
 const sitePageElement = document.querySelector('.page-body');
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -4,7 +4,17 @@ import { getRandomPoint } from '../mock/point.js';
 const POINT_COUNT = 5;
 
 export default class PointsModel extends Observable {
+  #pointsApiService = null;
   #points = Array.from({length: POINT_COUNT}, getRandomPoint);
+
+  constructor({pointsApiService}) {
+    super();
+    this.#pointsApiService = pointsApiService;
+
+    this.#pointsApiService.points.then((points) => {
+      console.log(points);
+    });
+  }
 
   get points() {
     return this.#points;
