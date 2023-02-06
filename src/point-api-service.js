fd0777cbd8f977ab2a65c41eb7a91fabe788e6e8
vertina61 <natalya.vertieva@mail.ru>
2023-02-06 23:07:15 +0300

    8.1.3 Реализует паттерн Адаптер

--- src/point-api-service.js
+++ src/point-api-service.js
@@ -1,4 +1,4 @@
-import ApiService from '../framework/api-service.js';
+import { ApiService } from '../framework/api-service.js';
 
 const Method = {
   GET: 'GET',
@@ -11,16 +11,30 @@ export default class PointsApiService extends ApiService {
       .then(ApiService.parseResponse);
   }
 
-  async updateTask(point) {
+  async updatePoint(point) {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(point) {
+    const adaptedPoint = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null
+    };
+
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+
+    return adaptedPoint;
   }
 }
